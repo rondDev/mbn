996cb96d4f6fb24d25e9d78446920dd689d1b2ff
rondDev <contact@rond.cc>
2025-04-09 00:21:48 +0200

    fix: janky command formatting thing

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,9 +1,5 @@
 local M = {}
 
-local function cmd(c)
-  return string.format("<Cmd>%s<CR>", c)
-end
-
 local function wrap(f)
   return function()
     f()
@@ -16,19 +12,19 @@ end
 -- ["<C-k>"] = { "<cmd>NvimTmuxNavigateUp<CR>", "Move up" },
 
 _G.actions = {
-  change_window_left = cmd("NvimTmuxNavigateLeft"),
-  change_window_right = cmd("NvimTmuxNavigateRight"),
-  change_window_up = cmd("NvimTmuxNavigateDown"),
-  change_window_down = cmd("NvimTmuxNavigateUp"),
-  close_window = cmd("wincmd q"),
-  move_window_left = cmd("wincmd H"),
-  move_window_right = cmd("wincmd L"),
-  move_window_up = cmd("wincmd K"),
-  move_window_down = cmd("wincmd J"),
+  change_window_left = "<cmd>NvimTmuxNavigateLeft<cr>",
+  change_window_right = "<cmd>NvimTmuxNavigateRight<cr>",
+  change_window_up = "<cmd>NvimTmuxNavigateDown<cr>",
+  change_window_down = "<cmd>NvimTmuxNavigateUp<cr>",
+  close_window = "<cmd>wincmd q<cr>",
+  move_window_left = "<cmd>wincmd H<cr>",
+  move_window_right = "<cmd>wincmd L<cr>",
+  move_window_up = "<cmd>wincmd K<cr>",
+  move_window_down = "<cmd>wincmd J<cr>",
 
-  split_window_vertically = cmd("vsplit"),
-  split_window_horizontally = cmd("split"),
-  window_quit = cmd("q"),
+  split_window_vertically = "<cmd>vsplit<cr>",
+  split_window_horizontally = "<cmd>split<cr>",
+  window_quit = "<cmd>q<cr>",
 
   go_start_of_line = "^",
   go_end_of_line = "$",
@@ -46,10 +42,10 @@ _G.actions = {
 
   redo = "<C-r>",
 
-  file_save = cmd("w"),
-  no_highlight = cmd("noh"),
+  file_save = "<cmd>w<cr>",
+  no_highlight = "<cmd>noh<cr>",
 
-  open_oil = cmd("Oil"),
+  open_oil = "<cmd>Oil<cr>",
   open_mini_files = wrap(function()
     local s = require("mini.files")
     if not s then
@@ -58,8 +54,8 @@ _G.actions = {
     s.open()
   end),
 
-  quit = cmd("q"),
-  write_quit = cmd("xa"),
+  quit = "<cmd>q<cr>",
+  write_quit = "<cmd>xa<cr>",
 
   toggle_sidebar = wrap(function()
     local s = require("sidebar-nvim")
@@ -129,7 +125,7 @@ function M.load_keys()
   map("n", "<leader>ts", actions.toggle_sidebar, { desc = "Toggle Sidebar" })
 
   -- window
-  map({ "n", "t" }, "<leader>wh", actions.change_window_left, { desc = "Move left" })
+  map({ "n", "t" }, "<leader>wh", actions.change_window_left)
   map({ "n", "t" }, "<leader>wl", actions.change_window_right, { desc = "Move right" })
   map({ "n", "t" }, "<leader>wk", actions.change_window_up, { desc = "Move up" })
   map({ "n", "t" }, "<leader>wj", actions.change_window_down, { desc = "Move down" })
