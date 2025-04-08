49a7eee271341fb2e322b049b38ca5457ddde266
rondDev <contact@rond.cc>
2025-04-08 23:48:18 +0200

    fix: formatting

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -22,19 +22,19 @@ return {
   {
     "HiPhish/rainbow-delimiters.nvim",
     main = "rainbow-delimiters.setup",
-    opts = {}
-  },
-  {
-    'stevearc/dressing.nvim',
     opts = {},
   },
   {
-    'echasnovski/mini.cursorword',
+    "stevearc/dressing.nvim",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.cursorword",
     version = false,
     opts = {},
   },
   {
-    'echasnovski/mini.files',
+    "echasnovski/mini.files",
     version = false,
     priority = 100,
     keys = {
@@ -48,7 +48,7 @@ return {
 
   },
   {
-    'echasnovski/mini.indentscope',
+    "echasnovski/mini.indentscope",
     version = false,
     opts = {},
   },
@@ -602,12 +602,10 @@ return {
           Snacks.toggle.diagnostics():map("<leader>ud")
           Snacks.toggle.line_number():map("<leader>ul")
           Snacks.toggle
-              .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-              :map("<leader>uc")
+            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+            :map("<leader>uc")
           Snacks.toggle.treesitter():map("<leader>uT")
-          Snacks.toggle
-              .option("background", { off = "light", on = "dark", name = "Dark Background" })
-              :map("<leader>ub")
+          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
           Snacks.toggle.inlay_hints():map("<leader>uh")
           Snacks.toggle.indent():map("<leader>ug")
           Snacks.toggle.dim():map("<leader>uD")
@@ -628,7 +626,7 @@ return {
     dependencies = { "neovim/nvim-lspconfig" },
     config = function()
       require("inlay-hints").setup()
-    end
+    end,
   },
   -- git integration for buffers
   {
@@ -682,7 +680,7 @@ return {
   {
     "NeogitOrg/neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim",  -- required
+      "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
 
       -- Only one of these is needed.
