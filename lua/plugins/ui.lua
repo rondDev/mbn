d0962586be2b3c930cbb8ac58f30f4df2e23ece2
rondDev <contact@rond.cc>
2025-04-08 23:56:10 +0200

    fix: add devicons and fix formatting

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,19 +1,20 @@
 return {
-  -- Better Quick Fix
-  { "kevinhwang91/nvim-bqf" },
-  -- Better notifications and LSP progress messages
-  {
-    "j-hui/fidget.nvim",
-    opts = {}
-  },
-  -- {
-  --   'nvimdev/dashboard-nvim',
-  --   event = 'VimEnter',
-  --   config = function()
-  --     require('dashboard').setup {
-  --       -- config
-  --     }
-  --   end,
-  --   dependencies = { { 'nvim-tree/nvim-web-devicons' } }
-  -- }
+	-- Better Quick Fix
+	{ "kevinhwang91/nvim-bqf" },
+	-- Better notifications and LSP progress messages
+	{
+		"j-hui/fidget.nvim",
+		opts = {},
+	},
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	-- {
+	--   'nvimdev/dashboard-nvim',
+	--   event = 'VimEnter',
+	--   config = function()
+	--     require('dashboard').setup {
+	--       -- config
+	--     }
+	--   end,
+	--   dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+	-- }
 }
