97b35dde18c6630dc16bcf98702fd0bb9166caf0
rondDev <contact@rond.cc>
2025-05-03 09:04:29 +0200

    feat: add rust plugin

--- lua/plugins/langs.lua
+++ lua/plugins/langs.lua
@@ -11,4 +11,12 @@ return {
     -- Uncomment next line if you want to follow only stable versions
     -- version = "*"
   },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^6", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rust_recommended_style = false
+    end,
+  },
 }
