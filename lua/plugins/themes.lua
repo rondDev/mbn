9fcd93a8e958ab9cb99529192a7e258fcac3b0a5
rondDev <contact@rond.cc>
2025-09-26 19:27:11 +0200

    fix: update packages and themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,14 +1,17 @@
 return {
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-  },
-  {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 10000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme("eldritch")
-    end,
-  },
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		config = function()
+			vim.cmd.colorscheme("oxocarbon")
+		end,
+	},
+	-- {
+	--   "eldritch-theme/eldritch.nvim",
+	--   lazy = false,
+	--   priority = 10000,
+	--   opts = {},
+	--   config = function()
+	--     vim.cmd.colorscheme("eldritch")
+	--   end,
+	-- },
 }
