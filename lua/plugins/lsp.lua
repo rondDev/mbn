6234b6a7c1175c1c87cb7da3f384f321886a7746
rondDev <contact@rond.cc>
2025-09-26 21:24:09 +0200

    fix: attempt to add cargo build for blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,9 @@ vim.pack.add({
   { src = "https://github.com/Saghen/blink.cmp" },
 })
 
+if not vim.fn.isdirectory('/home/rond/.local/share/nvim/site/pack/core/opt/blink.cmp/target') then
+  vim.system({'cd', '~/.local/share/nvim/site/pack/core/opt/blink.cmp', '&&', 'cargo', 'build', '--release'})
+end
 require("blink.cmp").setup({})
 
 vim.lsp.enable({"lua_ls", "rust_analyzer", "svelte" })
