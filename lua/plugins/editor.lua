df8a2a4f17cdd78e6ac68d7a214fc586e9376d2f
rondDev <contact@rond.cc>
2025-09-26 21:45:22 +0200

    fix: add flash

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,9 +2,17 @@ vim.pack.add({
   { src = "https://github.com/tpope/vim-surround" },
   { src = "https://github.com/tpope/vim-sleuth" },
   { src = "https://github.com/MysticalDevil/inlay-hints.nvim" },
-  { src = "https://github.com/vladdoster/remember.nvim" }
+  { src = "https://github.com/vladdoster/remember.nvim" },
+  { src = "https://github.com/folke/flash.nvim" },
 })
 
 
 require("inlay-hints").setup({})
 require("remember").setup({})
+require("flash").setup({
+  modes = {
+    char = {
+      jump_labels = true,
+    },
+  },
+})
