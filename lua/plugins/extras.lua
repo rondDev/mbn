fd57d022de9056a8aa0c7066b469bd1d189f71dd
rondDev <contact@rond.cc>
2025-09-26 20:26:56 +0200

    fix: update configuration of oil

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -5,6 +5,33 @@ vim.pack.add({
 
 })
 
-require("oil").setup({})
+require("oil").setup({
+      disable_default_keybindings = false,
+      columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = "actions.select_vsplit",
+        -- ["<C-h>"] = "actions.select_split", -- this is used to navigate left
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-r>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+      },
+      use_default_keymaps = false,
+      watch_for_changes = true,
+})
 require("which-key").setup({})
 require("neogit").setup({})
