33bddb46ff5ad8039e5299af9f1d65edac282367
rondDev <contact@rond.cc>
2025-09-26 21:45:53 +0200

    fix: add wakatime

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -2,7 +2,8 @@ vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/NeogitOrg/neogit" },
-  {	src = "https://github.com/alexghergh/nvim-tmux-navigation" },
+  { src = "https://github.com/alexghergh/nvim-tmux-navigation" },
+  { src = "https://github.com/wakatime/vim-wakatime" },
 
 })
 
