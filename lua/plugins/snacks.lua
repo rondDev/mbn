d12df2f68da781756c546ef07939461b73d72727
rondDev <contact@rond.cc>
2025-09-26 20:27:10 +0200

    fix: add snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,36 @@
+vim.pack.add({
+  { src = "https://github.com/folke/snacks.nvim",
+  },
+})
+
+require("snacks").setup({
+    bigfile = { enabled = false },
+    bufdelete = { enabled = true },
+    dashboard = { enabled = true },
+    debug = { enabled = true },
+    dim = { enabled = true },
+    explorer = { enabled = false },
+    git = { enabled = true },
+    gitbrowse = { enabled = true },
+    image = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    layout = { enabled = true },
+    lazygit = { enabled = false },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
+    picker = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+    styles = {
+      notification = {
+        -- wo = { wrap = true } -- Wrap notifications
+      },
+    },
+    zen = { enabled = true }, 
+  })
