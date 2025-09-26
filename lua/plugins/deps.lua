0c2a1a75b516e4c2b7ce9c81f63cf0a65cb9006c
rondDev <contact@rond.cc>
2025-09-26 20:01:33 +0200

    fix: add deps

---
+++ lua/plugins/deps.lua
@@ -0,0 +1,5 @@
+vim.pack.add({
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/sindrets/diffview.nvim" },
+  { src = "https://github.com/folke/snacks.nvim" },
+})
