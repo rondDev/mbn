d38aa30b2bc2319c23c29c47ef04fa462fd925b1
rondDev <contact@rond.cc>
2025-09-26 20:25:48 +0200

    fix: add more deps and initialize mini icons

--- lua/plugins/deps.lua
+++ lua/plugins/deps.lua
@@ -2,4 +2,8 @@ vim.pack.add({
   { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/sindrets/diffview.nvim" },
   { src = "https://github.com/folke/snacks.nvim" },
+  { src = "https://github.com/folke/snacks.nvim" },
+  { src = "https://github.com/nvim-mini/mini.icons" },
 })
+
+require("mini.icons").setup()
