d900f5c6de348887fdd9aa38c10c44e8b7100db5
rondDev <contact@rond.cc>
2025-09-26 20:37:38 +0200

    fix: add blink

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,8 @@
 vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/Saghen/blink.cmp" },
 })
+
+require("blink.cmp").setup({})
 
 vim.lsp.enable({"lua_ls", "rust_analyzer", "svelte" })
